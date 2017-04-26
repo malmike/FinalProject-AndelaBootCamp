0f5f354d465a95a6c52fe6886bf02a21e4ae6417
Male Michael <malmike21@outlook.com>
2017-04-26 14:28:11 +0300

    Split The Room Dictionaries To Office Dictionary And LivingSpace Dictionary

--- app/controller/dojo.py
+++ app/controller/dojo.py
@@ -1,9 +1,13 @@
+from app.models.room import Room
+
 class Dojo(object):
+    
     def __init__(self):
         self.people_dict = {}
-        self.room_dict = {}
+        self.office_dict = {}
+        self.living_space_dict = {}
     def create_room(self, room_name, room_type):
         pass
             
     def add_room(self, room_name, room_type):
-        pass
+        pass     
--- tests/test_dojo.py
+++ tests/test_dojo.py
@@ -7,7 +7,8 @@ class DojoTests(TestCase):
     def test_dojo_is_instance_of_Dojo(self):
         self.assertIsInstance(self.dojo, Dojo, "Object dojo is not an instance of class Dojo")
     def test_dojo_has_dict_of_rooms(self):
-        self.assertTrue(isinstance(self.dojo.room_dict,dict), "Class dojo doesnot contain any dictionary room_dict")
+        self.assertTrue(isinstance(self.dojo.living_space_dict,dict), "Class dojo doesnot contain any dictionary living_space_dict")
+        self.assertTrue(isinstance(self.dojo.office_dict,dict), "Class dojo doesnot contain any dictionary office_dict")
     def test_dojo_has_dict_of_people(self):
         self.assertTrue(isinstance(self.dojo.people_dict, dict), "Class dojo doesnot contain any dictionary people_dict")
     def test_add_room_and_repeated_room(self):
