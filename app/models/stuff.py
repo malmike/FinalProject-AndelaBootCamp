82a7ab18ce8dfcef58017440e321b6243a65ae7c
Male Michael <malmike21@outlook.com>
2017-04-26 05:21:48 +0300

    Made Stuff Class Inherit The Person Class

--- app/models/stuff.py
+++ app/models/stuff.py
@@ -1,4 +1,5 @@
 from app.models.person import Person
 class Stuff(Person):
     def __init__(self, name):
-        pass
+        position = "STUFF"
+        super(Stuff, self).__init__(name, position)
