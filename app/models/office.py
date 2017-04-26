9e9291cd1a08fa24541e7ae1ea76d5d5b262564c
Male Michael <malmike21@outlook.com>
2017-04-26 05:23:51 +0300

    Made Office Class Inherit Room Class

--- app/models/office.py
+++ app/models/office.py
@@ -1,4 +1,6 @@
 from app.models.room import Room
 class Office(Room):
     def __init__(self, name):
-        pass
+        capacity = 6
+        room_type = "OFFICE"
+        super(Office, self).__init__(name, capacity, room_type)
