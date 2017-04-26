ad90d8a4b89bfb9b000dae15d208279f96a925b5
Male Michael <malmike21@outlook.com>
2017-04-26 05:04:17 +0300

    Added Test To Check Name Attribute In Fellow Class And Stuff  Class

--- tests/test_person.py
+++ tests/test_person.py
@@ -18,11 +18,15 @@ class PersonTests(TestCase):
         self.assertTrue(issubclass(Fellow, Person), "Fellow class doesnot inherit from the Person class")
     def test_fellow_is_an_instance_of_Fellow(self):
         self.assertIsInstance(self.fellow, Fellow, "Object fellow is not an instance of the Fellow class")
+    def test_name_attribute_in_fellow(self):
+        self.assertEqual(self.fellow.name, self.name, "The value that is passed into name for fellow object and that returned, do not match")
     def test_fixed_attributes_in_fellow(self):
         self.asserEqual(self.fellow.position, 'FELLOW', "Fellow doesnot contain any attribute position")
     def test_stuff_inherits_person(self):
         self.assertTrue(issubclass(Stuff, Person), "Stuff class doesnot inherit from the Person class")
     def test_stuff_is_instance_of_Stuff(self):
         self.assertIsInstance(self.stuff, Stuff, "Object stuff is not an instance of class Stuff")
+    def test_name_attribute_in_stuff(self):
+        self.assertEqual(self.stuff.name, self.name, "The value that is passed into name for stuff object and that returned, do not match")
     def test_fixed_attributes_in_stuff(self):
         self.asserEqual(self.stuff.position, 'STUFF', "Stuff doesnot contain any attribute position")
