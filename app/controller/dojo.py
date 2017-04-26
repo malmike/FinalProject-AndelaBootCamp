fd301737610c89fb964f870d5ec53632f42f31b0
Male Michael <malmike21@outlook.com>
2017-04-26 14:19:42 +0300

    Added Empty add_room Method To Dojo Class

--- app/controller/dojo.py
+++ app/controller/dojo.py
@@ -2,5 +2,8 @@ class Dojo(object):
     def __init__(self):
         self.people_dict = {}
         self.room_dict = {}
-    def create_room(self):
+    def create_room(self, room_name, room_type):
+        pass
+            
+    def add_room(self, room_name, room_type):
         pass
