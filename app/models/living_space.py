73f31ace3c61d6f58856c397590e045b4cc34999
Male Michael <malmike21@outlook.com>
2017-04-26 11:40:50 +0300

    Added Person To The allocateion_list List In LivingSpace Class

--- app/models/living_space.py
+++ app/models/living_space.py
@@ -5,11 +5,15 @@ class LivingSpace(Room):
     #Initialise the attributes of the LivingSpace Class
     def __init__(self, name):
         self.allocation_list = []
-        capacity = 4
+        self.capacity = 4
         room_type = "LIVINGSPACE"
         #Make call to base class Room
         super(LivingSpace, self).__init__(name, room_type)
     def add_person(self, person):
-        pass
+        if len(self.allocation_list) < self.capacity:
+            self.allocation_list.append(person)
+            return True
+        else:
+            return False
     def get_allocate_len(self):
         pass
