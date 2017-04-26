f40ac0e330fc4ec77540d0dfd97516d01ab43baa
Male Michael <malmike21@outlook.com>
2017-04-26 06:04:34 +0300

    Imported DojoTests Class Into The run_tests File

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -1,6 +1,7 @@
 import unittest
 from tests.test_person import PersonTests
 from tests.test_room import RoomTests
+from tests.test_dojo import DojoTests
 
 if __name__ == "__main__":
     unittest.main()
