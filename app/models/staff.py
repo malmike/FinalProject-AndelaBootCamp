1e2b8551827032452f6eab484ef3a5f95e71fa2d
Male Michael <malmike21@outlook.com>
2017-04-26 19:02:14 +0300

    Updated Fellow And Staff Class To Inherit From The Abstruct Class Person

--- app/models/staff.py
+++ app/models/staff.py
@@ -3,7 +3,4 @@ from app.models.person import Person
 #Let Staff Inherit Person Class
 class Staff(Person):
     #Initialise the attributes of the Staff Class
-    def __init__(self, name):
-        position = "STAFF"
-        #Make a call to base Class Person
-        super(Staff, self).__init__(name, position)
+    position = "STAFF"
