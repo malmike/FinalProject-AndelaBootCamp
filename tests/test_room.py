aa88538c4296c4139ca83204f3dfd5b8ecb3be39
Male Michael <malmike21@outlook.com>
2017-04-26 03:42:24 +0300

    Added Tests For Office Class

--- tests/test_room.py
+++ tests/test_room.py
@@ -14,4 +14,12 @@ class RoomTests(TestCase):
         self.assertEqual(self.room.name, self.name, "The value that is passed into name and that returned, do not match")
         self.assertEqual(self.room.capacity, self.capacity, "The value that is passed into capacity and that returned, do not match")
         self.assertEqual(self.room.room_type, self.room_type, "The value that is passed into room_type and that returned, do not match")
-   
+    def test_office_inherits_room(self):
+        self.assertTrue(issubclass(Office, Room), "Office class doesnot inherit from the Room class")
+    def test_office_is_an_instance_of_Office(self):
+        self.assertIsInstance(self.office, Office, "Object office is not an instance of the Office class")
+    def test_fixed_attributes_in_office(self):
+        self.assertEqual(self.office.capacity, 6, "Office doesnot contain any attribute capacity")
+        self.asserEqual(self.office.type, 'OFFICE', "Object doesnot contain any attribute type")
+    
+    
