516d5a705553f0753354cf7d319f7177b5da4679
Male Michael <malmike21@outlook.com>
2017-04-26 20:26:04 +0300

    Added A Method To Create A Room

--- app/controller/dojo.py
+++ app/controller/dojo.py
@@ -8,17 +8,17 @@ class Dojo(object):
         self.office_dict = {}
         self.living_space_dict = {}
     def create_room(self, room_name, room_type):
-        pass
-            
-    def add_room(self, room_name, room_type):
-        if room_type == "office" and room_name not in self.office_dict:
-            office = Office(room_name)
-            self.office_dict[room_name] = office
-            return True
-        elif room_type == "living_space" and room_name not in self.living_space_dict:
-            living_space = LivingSpace(room_name)
-            self.living_space_dict[room_name] = living_space
-            return True
+        if isinstance(room_name, str) and isinstance(room_type, str):
+            if room_type == "office" and room_name not in self.office_dict:
+                office = Office(room_name)
+                self.office_dict[room_name] = office
+                return True
+            elif room_type == "living_space" and room_name not in self.living_space_dict:
+                living_space = LivingSpace(room_name)
+                self.living_space_dict[room_name] = living_space
+                return True
+            else:
+                return False
         else:
             return False
                  
