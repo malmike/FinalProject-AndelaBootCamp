9df4d030fa154363e6db88bdc153dc6016c9fe21
Male Michael <malmike21@outlook.com>
2017-04-26 15:25:04 +0300

    Resolved Errors That Were In The Add Room Test

--- tests/test_dojo.py
+++ tests/test_dojo.py
@@ -12,22 +12,37 @@ class DojoTests(TestCase):
     def test_dojo_has_dict_of_people(self):
         self.assertTrue(isinstance(self.dojo.people_dict, dict), "Class dojo doesnot contain any dictionary people_dict")
     def test_add_room_and_repeated_room(self):
-        #Check whether one room can be created
-        initial_room_count = len(self.dojo.room_dict)
-        blue_office = self.dojo.create_room('Blue', 'office')
+        #Check whether one office room can be created
+        initial_office_count = len(self.dojo.office_dict)
+        blue_office = self.dojo.add_room('Blue', 'office')
         self.assertTrue(blue_office, "Room has not been created")
-        new_room_count = len(self.dojo.room_dict)
-        self.assertEqual(new_room_count - initial_room_count, 1, "Multiple values are being added to the dictionary. Why is that!!!")
-        #Check whether the same room can be created
-        blue_office2 = self.dojo.create_room('Blue', 'office')
+        new_office_count = len(self.dojo.office_dict)
+        self.assertEqual(new_office_count - initial_office_count, 1, "Multiple values are being added to the office dictionary. Why is that!!!")
+        #Check whether the same office room can be created
+        initial_office_count = len(self.dojo.office_dict)
+        blue_office2 = self.dojo.add_room('Blue', 'office')
         self.assertFalse(blue_office2, "Repeated room being created")
-        new_room_count = len(self.dojo.room_dict)
-        self.assertEqual(new_room_count - initial_room_count, 0, "The value is being added to the dictionary when it should not")
-    def test_create_room_successfully(self):
-        initial_room_count = len(self.dojo.room_dict)
-        blue_office = self.dojo.create_room('Blue', 'office')
-        self.assertTrue(blue_office)
-        new_room_count = len(self.dojo.room_dict)
-        self.assertEqual(new_room_count - initial_room_count, 1, "Multiple values are being added to the dictionary. Why is that!!!")
+        new_office_count = len(self.dojo.office_dict)
+        self.assertEqual(new_office_count - initial_office_count, 0, "The value is being added to the office dictionary when it should not")
+
+        #Check whether one living space room can be created
+        initial_room_count = len(self.dojo.living_space_dict)
+        blue_living_space = self.dojo.add_room('Blue', 'living_space')
+        self.assertTrue(blue_living_space, "Room has not been created")
+        new_room_count = len(self.dojo.living_space_dict)
+        self.assertEqual(new_room_count - initial_room_count, 1, "Multiple values are being added to the living space dictionary. Why is that!!!")
+        #Check whether the same living space room can be created
+        initial_room_count = len(self.dojo.living_space_dict)
+        blue_living_space2 = self.dojo.add_room('Blue', 'living_space')
+        self.assertFalse(blue_living_space2, "Repeated room being created")
+        new_room_count = len(self.dojo.office_dict)
+        self.assertEqual(new_room_count - initial_room_count, 0, "The value is being added to the living space dictionary when it should not")
+
+    # def test_create_room_successfully(self):
+    #     initial_room_count = len(self.dojo.office_dict)
+    #     blue_office = self.dojo.create_room('Blue', 'office')
+    #     self.assertTrue(blue_office)
+    #     new_room_count = len(self.dojo.office_dict)
+    #     self.assertEqual(new_room_count - initial_room_count, 1, "Multiple values are being added to the dictionary. Why is that!!!")
     
     
