a160768761b63d2b6af75288565c2b7ae6890178
Male Michael <malmike21@outlook.com>
2017-04-26 19:04:19 +0300

    Updated The LivingSpace Class And The Office Class To Inherit From The Abstract Class Room

--- app/models/living_space.py
+++ app/models/living_space.py
@@ -3,27 +3,5 @@ from app.models.room import Room
 #Let LivingSpace inherit the Room Class
 class LivingSpace(Room):
     #Initialise the attributes of the LivingSpace Class
-    def __init__(self, name):
-        self.allocation_list = []
-        self.capacity = 4
-        room_type = "LIVINGSPACE"
-        #Make call to base class Room
-        super(LivingSpace, self).__init__(name, room_type)
-    def add_person(self, person):
-        if len(self.allocation_list) < self.capacity:
-            self.allocation_list.append(person)
-            return True
-        else:
-            return False
-    def get_allocate_len(self):
-        return len(self.allocation_list)
-    def room_assignable(self):
-        if len(self.allocation_list) < self.capacity:
-            return True
-        else:
-            return False
-    def is_room_assignable(self):
-        if len(self.allocation_list) < self.capacity:
-            return True
-        else:
-            return False
+    capacity = 4
+    room_type = "LIVINGSPACE"
