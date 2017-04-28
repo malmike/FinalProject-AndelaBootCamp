a8cd1b35e24ccf7d3d396d1ee55234e06fe94e03
Male Michael <malmike21@outlook.com>
2017-04-28 10:02:28 +0300

    Added The Database Schema Based On SQLAlchemy Specifications

---
+++ app/database_methods/db_schema.py
@@ -0,0 +1,27 @@
+from sqlalchemy import MetaData
+from sqlalchemy import Table, Column
+from sqlalchemy import Integer, String
+from sqlalchemy import ForeignKey
+
+
+class DbSchema(object):
+    def __init__(self):
+        self.metadata = MetaData()
+        self.office = Table('office', self.metadata, Column('name', String, primary_key=True))
+        self.livingspace = Table('livingspace', self.metadata, Column('name', String, primary_key=True))
+        self.staff = Table('staff', self.metadata, Column('name', String, primary_key=True))
+        self.fellow = Table('fellow', self.metadata, Column('name', String, primary_key=True))
+        self.office_allocation = Table('office_allocation', self.metadata, 
+            Column('id', Integer, primary_key=True), 
+            Column('staff_name', String, ForeignKey('staff.name')),
+            Column('fellow_name', String, ForeignKey('fellow.name')),
+            Column('office_name', String, ForeignKey('office.name'))            
+            )
+        self.livingspace_allocation = Table('livingspace_allocation', self.metadata, 
+            Column('id', Integer, primary_key=True), 
+            Column('fellow_name', String, ForeignKey('fellow.name')),
+            Column('livingspace_name', String, ForeignKey('livingspace.name'))            
+            )
+
+        
+
