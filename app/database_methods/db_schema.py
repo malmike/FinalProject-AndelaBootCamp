cf446928a8cf470a028f702628c2e9137daa4534
Male Michael <malmike21@outlook.com>
2017-04-28 10:51:21 +0300

    Added Descriptor To The DbSchema Class

--- app/database_methods/db_schema.py
+++ app/database_methods/db_schema.py
@@ -2,10 +2,14 @@ from sqlalchemy import MetaData
 from sqlalchemy import Table, Column
 from sqlalchemy import Integer, String
 from sqlalchemy import ForeignKey
-
+"""
+These are file meant to allow mapping of the database tables to the class
+It is using sqlalchemy as an orm for the attributes to tables in the database
+"""
 
 class DbSchema(object):
     def __init__(self):
+        #Mapping variables to the database tables
         self.metadata = MetaData()
         self.office = Table('office', self.metadata, Column('name', String, primary_key=True))
         self.livingspace = Table('livingspace', self.metadata, Column('name', String, primary_key=True))
