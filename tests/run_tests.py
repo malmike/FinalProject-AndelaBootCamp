1c9d07537f4176b3a44fc19cf7b6360a001daf1b
Male Michael <malmike21@outlook.com>
2017-04-28 10:00:50 +0300

    Added Call to the DatabaseTest Class

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -2,6 +2,7 @@ import unittest
 from tests.test_person import PersonTests
 from tests.test_room import RoomTests
 from tests.test_dojo import DojoTests
+from tests.test_db import DatabaseTest
 
 if __name__ == "__main__":
     unittest.main()
