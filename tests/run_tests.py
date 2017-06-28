18958f6daec3dc02f393d53585a9822d9dcbb132
Male Michael <malmike21@outlook.com>
2017-06-28 22:37:33 +0300

    Made changes to the order in which the tests are run

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -4,8 +4,8 @@ from tests.test_room import RoomTests
 from tests.test_add_room import AddRoomTests
 from tests.test_add_person import AddPersonTests
 from tests.test_dojo import DojoTests
-from tests.test_db import DatabaseTest
 from tests.test_allocations import AllocationsTests
+from tests.test_db import DatabaseTest
 
 
 if __name__ == "__main__":
