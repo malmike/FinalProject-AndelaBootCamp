78ba1b819660f7ab41b3c2ee71d2c2e179e7498e
Male Michael <malmike21@outlook.com>
2017-07-01 12:28:31 +0300

    Changed the run_tests file to the parent direectory

---
+++ run_tests.py
@@ -0,0 +1,14 @@
+import unittest
+from tests.test_person import PersonTests
+from tests.test_room import RoomTests
+from tests.test_add_room import AddRoomTests
+from tests.test_add_person import AddPersonTests
+from tests.test_dojo import DojoTests
+from tests.test_allocations import AllocationsTests
+from tests.test_reallocation import ReallocationsTests
+from tests.test_load_people import LoadPeopleTests
+from tests.test_db import DatabaseTest
+
+
+if __name__ == "__main__":
+    unittest.main()
