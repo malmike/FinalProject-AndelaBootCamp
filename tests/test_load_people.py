6b2eebbca08b5f7a9336f14e7f2a943bf73c6cbc
Male Michael <malmike21@outlook.com>
2017-07-01 12:26:57 +0300

    Added file containing tests for load people function

---
+++ tests/test_load_people.py
@@ -0,0 +1,11 @@
+from unittest import TestCase
+from app.controller.dojo import Dojo
+
+class LoadPeopleTests(TestCase):
+
+
+    #Set up the various class variables to be used during during tests
+    def setUp(self):
+        self.dojo = Dojo()
+
+    
