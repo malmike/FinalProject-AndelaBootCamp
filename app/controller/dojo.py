e9ba993ee21045950928afa4e5d146590ec772c4
Male Michael <malmike21@outlook.com>
2017-07-02 06:28:04 +0300

    Removing of nested ifs

--- app/controller/dojo.py
+++ app/controller/dojo.py
@@ -24,88 +24,93 @@ class Dojo(object):
 
     #Method to create a room
     def create_room(self, room_type, room_name):
-        if isinstance(room_type, str):
-            #Create an office room and add it to office list
-            if room_type == "OFFICE" and room_name not in self.office_dict and room_name not in self.living_space_dict:
-                office = Office(room_name)
-                self.office_dict[room_name] = office
-                return office
-            #Create livingspace room and add it to livingspace list
-            elif room_type == "LIVINGSPACE" and room_name not in self.living_space_dict and room_name not in self.office_dict:
-                living_space = LivingSpace(room_name)
-                self.living_space_dict[room_name] = living_space
-                return living_space
-            else:
-                return False
-        else:
+        if not isinstance(room_type, str):
             raise TypeError('Values inserted must both be strings')
+
+        #Create an office room and add it to office list
+        if room_type == "OFFICE" and room_name not in self.office_dict and room_name not in self.living_space_dict:
+            office = Office(room_name)
+            self.office_dict[room_name] = office
+            return office
+        #Create livingspace room and add it to livingspace list
+        elif room_type == "LIVINGSPACE" and room_name not in self.living_space_dict and room_name not in self.office_dict:
+            living_space = LivingSpace(room_name)
+            self.living_space_dict[room_name] = living_space
+            return living_space
+        else:
+            return False
+            
 
 
     #Add a method to add a person
     def add_person(self, person_type, person_name):
-        if isinstance(person_type, str):
-            #Add a fellow and add to the fellow list
-            if person_type == "FELLOW" and person_name not in self.fellow_dict and person_name not in self.staff_dict:
-                fellow = Fellow(person_name)
-                self.fellow_dict[person_name] = fellow
-                return fellow
-            #Add a staff and add to the staff list
-            elif person_type == "STAFF" and person_name not in self.staff_dict and person_name not in self.fellow_dict:
-                staff = Staff(person_name)
-                self.staff_dict[person_name] = staff
-                return staff
-            else:
-                return False
-        else:
+        
+        if not isinstance(person_type, str):
             raise TypeError('Values inserted must both be strings')
+
+        #Add a fellow and add to the fellow list
+        if person_type == "FELLOW" and person_name not in self.fellow_dict and person_name not in self.staff_dict:
+            fellow = Fellow(person_name)
+            self.fellow_dict[person_name] = fellow
+            return fellow
+        #Add a staff and add to the staff list
+        elif person_type == "STAFF" and person_name not in self.staff_dict and person_name not in self.fellow_dict:
+            staff = Staff(person_name)
+            self.staff_dict[person_name] = staff
+            return staff
+        else:
+            return False
+            
 
 
     #Method to get unallocated rooms
     def get_unallocated_rooms(self, room_type):
         unallocated_rooms = []
-        if isinstance(room_type, str):
-            #sort though the office/ living space dictionary and get unallocated rooms
-            if room_type.upper() == 'OFFICE':
-                for room in self.office_dict:
-                    if self.office_dict[room].is_room_assignable():
-                        unallocated_rooms.append(room)
-            elif room_type.upper() == 'LIVINGSPACE':
-                for room in self.living_space_dict:
-                    if self.living_space_dict[room].is_room_assignable():
-                        unallocated_rooms.append(room)
-            else:
-                raise ValueError('Room type entered must be OFFICE or LIVINGSPACE')
-
-            return unallocated_rooms
-            
-        else:
+        if not isinstance(room_type, str):
             raise TypeError('Room Type must be a string')
+
+        #sort though the office/ living space dictionary and get unallocated rooms
+        if room_type.upper() == 'OFFICE':
+            for room in self.office_dict:
+                if self.office_dict[room].is_room_assignable():
+                    unallocated_rooms.append(room)
+        elif room_type.upper() == 'LIVINGSPACE':
+            for room in self.living_space_dict:
+                if self.living_space_dict[room].is_room_assignable():
+                    unallocated_rooms.append(room)
+        else:
+            raise ValueError('Room type entered must be OFFICE or LIVINGSPACE')
+
+        return unallocated_rooms
+
+            
 
 
     #Method to allocate rooms at random, it takes in a person objec and room type
     def allocate_rooms(self, person_object, room_type):
         unallocated_rooms = self.get_unallocated_rooms(room_type)
 
-        if unallocated_rooms:
-            if room_type == "OFFICE":
-                index = random.choice(range(len(unallocated_rooms)))
-                value = self.office_dict[unallocated_rooms[index]].add_person(person_object)
-                if not value:
-                    return self.allocate_rooms(person_object, room_type)
-                else:
-                    return unallocated_rooms[index]
-            elif room_type == "LIVINGSPACE":
-                index = random.choice(range(len(unallocated_rooms)))
-                value = self.living_space_dict[unallocated_rooms[index]].add_person(person_object)
-                if not value:
-                    return self.allocate_rooms(person_object, room_type)
-                else:
-                    return unallocated_rooms[index]
-            else:
-                raise ValueError('Room type entered must be OFFICE or LIVINGSPACE')
-        else:
+        if not unallocated_rooms:
             self.unallocated_people[room_type.upper()].append(person_object)
             return False
+
+        if room_type == "OFFICE":
+            index = random.choice(range(len(unallocated_rooms)))
+            value = self.office_dict[unallocated_rooms[index]].add_person(person_object)
+            if not value:
+                return self.allocate_rooms(person_object, room_type)
+            else:
+                return unallocated_rooms[index]
+        elif room_type == "LIVINGSPACE":
+            index = random.choice(range(len(unallocated_rooms)))
+            value = self.living_space_dict[unallocated_rooms[index]].add_person(person_object)
+            if not value:
+                return self.allocate_rooms(person_object, room_type)
+            else:
+                return unallocated_rooms[index]
+        else:
+            raise ValueError('Room type entered must be OFFICE or LIVINGSPACE')
+            
 
 
     #Method to get room occupants
@@ -150,106 +155,104 @@ class Dojo(object):
 
     #Method to retrieve the room an individual is assigned
     def get_room_assigned(self, room_type, person_name):
-        if isinstance(room_type, str) and isinstance(person_name, str):
-            if room_type.upper() == 'OFFICE':
-                for person in self.unallocated_people['OFFICE']:
-                    if person.name == person_name:
-                        return { "person": person, "room": None }
-                
-                for room in self.office_dict:
-                    if self.office_dict[room].allocation_list:
-                        for person in self.office_dict[room].allocation_list:
-                            if person.name == person_name:
-                                return { "person": person, "room": room }
-
-                return False
-                
-            elif room_type.upper() == 'LIVINGSPACE':
-                for person in self.unallocated_people['LIVINGSPACE']:
-                    if person.name == person_name:
-                        return { "person": person, "room": None }
-                
-                
-                for room in self.living_space_dict:
-                    if self.living_space_dict[room].allocation_list:
-                        for person in self.living_space_dict[room].allocation_list:
-                            if person.name == person_name:
-                                return { "person": person, "room": room }
-                
-                return False
-
-            else:
-                raise ValueError('Room Type must either be OFFICE or LIVINGSPACE')
-
-        else:
+        if not isinstance(room_type, str) and not isinstance(person_name, str):
             raise TypeError('Room Type must be a string')
 
+        if room_type.upper() == 'OFFICE':
+            for person in self.unallocated_people['OFFICE']:
+                if person.name == person_name:
+                    return { "person": person, "room": None }
+            
+            for room in self.office_dict:
+                if self.office_dict[room].allocation_list:
+                    for person in self.office_dict[room].allocation_list:
+                        if person.name == person_name:
+                            return { "person": person, "room": room }
+
+            return False    
+        elif room_type.upper() == 'LIVINGSPACE':
+            for person in self.unallocated_people['LIVINGSPACE']:
+                if person.name == person_name:
+                    return { "person": person, "room": None }
+            
+            
+            for room in self.living_space_dict:
+                if self.living_space_dict[room].allocation_list:
+                    for person in self.living_space_dict[room].allocation_list:
+                        if person.name == person_name:
+                            return { "person": person, "room": room }
+            
+            return False
+        else:
+            raise ValueError('Room Type must either be OFFICE or LIVINGSPACE')     
 
 
     #Method to unallocate someone from a room
     def unallocate_room(self, room_type, room_allocated, person_object):
-        if isinstance(room_type, str) and isinstance(room_allocated, str):
-            if room_type.upper() == 'LIVINGSPACE':
-                self.living_space_dict[room_allocated].allocation_list.remove(person_object)
-            elif room_type.upper() == 'OFFICE':
-                self.office_dict[room_allocated].allocation_list.remove(person_object)
-            else:
-                raise ValueError('Room Type should be either LIVINGSPACE or OFFICE')
-        else:
+        if not isinstance(room_type, str) and not isinstance(room_allocated, str):
             raise TypeError('Room Type and Room Allocated should be strings')
+
+        if room_type.upper() == 'LIVINGSPACE':
+            self.living_space_dict[room_allocated].allocation_list.remove(person_object)
+        elif room_type.upper() == 'OFFICE':
+            self.office_dict[room_allocated].allocation_list.remove(person_object)
+        else:
+            raise ValueError('Room Type should be either LIVINGSPACE or OFFICE')
+            
 
 
     #Method to assign some one to a room
     def assign_individual_room(self, room_type, room_name, person_object):
-        if isinstance(room_type, str) and isinstance(room_name, str):
-            if room_type.upper() == 'LIVINGSPACE':
-                if not self.living_space_dict[room_name].is_room_assignable():
-                    self.unallocated_people[room_type.upper()].append(person_object)
-                    return False
-                else:
-                    value = self.living_space_dict[room_name].add_person(person_object)
-                    return value
-
-            elif room_type.upper() == 'OFFICE':
-                if not self.office_dict[room_name].is_room_assignable():
-                    self.unallocated_people[room_type.upper()].append(person_object)
-                    return False
-                else:
-                    value = self.office_dict[room_name].add_person(person_object)
-                    return value                  
-
-            else:
-                raise ValueError('Room Type should be either LIVINGSPACE or OFFICE')
-        else:
+        if not isinstance(room_type, str) and not isinstance(room_name, str):
             raise TypeError('Room Type and Room Name must be of type string')
+
+        if room_type.upper() == 'LIVINGSPACE':
+            if not self.living_space_dict[room_name].is_room_assignable():
+                self.unallocated_people[room_type.upper()].append(person_object)
+                return False
+            else:
+                value = self.living_space_dict[room_name].add_person(person_object)
+                return value
+
+        elif room_type.upper() == 'OFFICE':
+            if not self.office_dict[room_name].is_room_assignable():
+                self.unallocated_people[room_type.upper()].append(person_object)
+                return False
+            else:
+                value = self.office_dict[room_name].add_person(person_object)
+                return value                  
+
+        else:
+            raise ValueError('Room Type should be either LIVINGSPACE or OFFICE')
+            
 
     
     #Method to reallocate someones room
     def reallocate_room(self, person_name, room_name):
-        if isinstance(room_name, str) and isinstance(person_name, str):
-            #Get the room type
-            room_type = self.get_room_type(room_name)
-            if room_type == 'LIVINGSPACE':
-                #Check if the room is assignable
-                if self.living_space_dict[room_name].is_room_assignable:
-                    return self.reassign_room(room_type, room_name, person_name)        
-                else:
-                    return 'The room specified is not assignable'
-
-            elif room_type == 'OFFICE':
-                #Check if the room is assignable
-                if self.office_dict[room_name].is_room_assignable:
-                    return self.reassign_room(room_type, room_name, person_name)
-                    
-                else:
-                    return 'The room specified is not assignable'
-
-            else:
-                return 'The room '+room_name+' does not exist'
-
-        else:
+        if not isinstance(room_name, str) and not isinstance(person_name, str):
             raise TypeError('Room Type and Person Name must be of type string')
 
+        #Get the room type
+        room_type = self.get_room_type(room_name)
+        if room_type == 'LIVINGSPACE':
+            #Check if the room is assignable
+            if self.living_space_dict[room_name].is_room_assignable:
+                return self.reassign_room(room_type, room_name, person_name)        
+            else:
+                return 'The room specified is not assignable'
+
+        elif room_type == 'OFFICE':
+            #Check if the room is assignable
+            if self.office_dict[room_name].is_room_assignable:
+                return self.reassign_room(room_type, room_name, person_name)
+                
+            else:
+                return 'The room specified is not assignable'
+
+        else:
+            return 'The room '+room_name+' does not exist'
+
+            
 
     #Method for reassigning room
     def reassign_room(self, room_type, room_name, person_name):
