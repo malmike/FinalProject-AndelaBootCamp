a7cebb52f076a66742d095e4d734c5a95aec7d14
Male Michael <malmike21@outlook.com>
2017-07-03 12:51:59 +0300

    Added tests for get_dict method

--- tests/test_dojo.py
+++ tests/test_dojo.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from app.controller.dojo import Dojo
 
 class DojoTests(TestCase):
-    
+
 
     #Test that when the class Dojo is instantiated, the object that is created is an instance of the Dojo class
     def test_dojo_is_instance_of_Dojo(self):
@@ -23,10 +23,52 @@ class DojoTests(TestCase):
         self.assertTrue(isinstance(dojo.staff_dict, dict), "Class dojo doesnot contain any dictionary staff_dict")
         self.assertTrue(isinstance(dojo.fellow_dict, dict), "Class dojo doesnot contain any dictionary fellow_dict")
 
-    
+
     #Test check for strings
     def test_check_str(self):
         dojo = Dojo()
         with self.assertRaises(TypeError):
             dojo.check_str(RoomType=1)
-    
+
+    #Test to get office dictonary when room type OFFICE is passed
+    def test_get_office_dict_(self):
+        dojo = Dojo()
+        office_dict = dojo.get_dict("ROOM", "OFFICE")
+        self.assertEqual(
+            office_dict,
+            dojo.office_dict,
+            "Dictionary returned should be the same as the office_dict in dojo"
+        )
+
+
+    #Test to get living space dictonary when room type LIVINGSPACE is passed
+    def test_get_living_space_dict_(self):
+        dojo = Dojo()
+        living_space_dict = dojo.get_dict("ROOM", "LIVINGSPACE")
+        self.assertEqual(
+            living_space_dict,
+            dojo.living_space_dict,
+            "Dictionary returned should be the same as the office_dict in dojo"
+        )
+
+
+    #Test to get staff dictonary when person type STAFF is passed
+    def test_get_staff_dict_(self):
+        dojo = Dojo()
+        staff_dict = dojo.get_dict("PERSON", "STAFF")
+        self.assertEqual(
+            staff_dict,
+            dojo.staff_dict,
+            "Dictionary returned should be the same as the sta in dojo"
+        )
+
+
+    #Test to get office dictonary when room type OFFICE is passed
+    def test_get_fellow_dict_(self):
+        dojo = Dojo()
+        fellow_dict = dojo.get_dict("PERSON", "FELLOW")
+        self.assertEqual(
+            fellow_dict,
+            dojo.fellow_dict,
+            "Dictionary returned should be the same as the office_dict in dojo"
+        )
