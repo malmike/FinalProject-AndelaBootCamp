7a067baf733eec68b8ef7eea826f20f47beb9604
Male Michael <malmike21@outlook.com>
2017-07-22 16:40:48 +0300

    Modified the run_tests file Improved on the structure in which the tests are imported and run in the run_tests.py file

--- run_tests.py
+++ run_tests.py
@@ -1,14 +1,43 @@
 import unittest
-from tests.test_person import PersonTests
-from tests.test_room import RoomTests
-from tests.test_add_room import AddRoomTests
-from tests.test_add_person import AddPersonTests
-from tests.test_dojo import DojoTests
-from tests.test_allocations import AllocationsTests
-from tests.test_reallocation import ReallocationsTests
-from tests.test_load_people import LoadPeopleTests
-from tests.test_db import DatabaseTest
+from tests import test_person, test_room, test_add_room
+from tests import test_add_person, test_dojo
+from tests import test_allocations, test_reallocation
+from tests import test_load_people, test_db
 
+suite = unittest.TestLoader().loadTestsFromTestCase(
+    test_person.PersonTests
+)
 
-if __name__ == "__main__":
-    unittest.main()
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(
+    test_room.RoomTests
+))
+
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(
+    test_add_room.AddRoomTests
+))
+
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(
+    test_add_person.AddPersonTests
+))
+
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(
+    test_dojo.DojoTests
+))
+
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(
+    test_allocations.AllocationsTests
+))
+
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(
+    test_reallocation.ReallocationsTests
+))
+
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(
+    test_load_people.LoadPeopleTests
+))
+
+suite.addTest(unittest.TestLoader().loadTestsFromTestCase(
+    test_db.DatabaseTest
+))
+
+unittest.TextTestRunner(verbosity=2).run(suite)
